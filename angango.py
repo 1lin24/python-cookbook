735620e78549eff9da9e2afe179ed401eedbac3d
phyng <phyngk@gmail.com>
2015-07-14 19:07:49 +0800

    Add parse ng_href.

--- angango.py
+++ angango.py
@@ -10,8 +10,16 @@ from pyquery import PyQuery as pq
 
 class Angango(object):
 
+    ATTR_BLACKLIST = ['ng-click', 'ng-class', 'ng-attr-class', 'edit-image', 'edit-text']
+
     def __init__(self, html):
         self.d = pq(html)
+
+    def _extend_attr(self, attrs):
+        for attr in attrs:
+            yield attr
+            if not attr.startswith('data-'):
+                yield 'data-' + attr
 
     def parse_ng_repeat(self):
         PATTERN = re.compile(r'^\s*([a-zA-Z][a-zA-Z0-9_]*)\s+in\s+([a-zA-Z][a-zA-Z0-9_]*)\s*$')
@@ -38,14 +46,30 @@ class Angango(object):
                 pq_ele.remove_attr(attr)
         return self.d
 
+    def parse_ng_href(self):
+        PATTERN = re.compile(r'\{\{([^\{\}]*)\}\}')
+        for attr in ['ng-href', 'data-ng-href']:
+            for ele in self.d('[{}]'.format(attr)):
+                pq_ele = pq(ele)
+                attr_value = pq_ele.attr(attr)
+                dj_if = ' and '.join([i.strip() for i in PATTERN.findall(attr_value)])
+                print '{{% if {} %}}{}{{% endif %}}'.format(dj_if, 'href="{}"'.format(attr_value))
+
+
+    def parse_ng_src(self):
+        pass
+
+    def parse_ng_attr_target(self):
+        pass
+
     def remove_attr(self):
-        attrs = ['ng-click', 'ng-class', 'ng-attr-class']
-        for attr in attrs:
+        for attr in self._extend_attr(self.ATTR_BLACKLIST):
             self.d('[{}]'.format(attr)).remove_attr(attr)
 
     def parse_all(self):
         self.parse_ng_repeat()
         self.parse_ng_style()
+        self.parse_ng_href()
         self.remove_attr()
         return self.d
 
@@ -62,8 +86,14 @@ if __name__ == '__main__':
                 </div>
             </div>
             <button ng-click="save()">save</button>
+            <div class="s-image1">
+                <a data-edit-image="slide.components.image1"
+                    ng-href="http://{{ slide.components.image1.href }}/{{ pk }}" ng-attr-target="{{ slide.components.image1.target }}">
+                    <img ng-src="{{ slide.components.image1.value|qiniu }}">
+                </a>
+            </div>
         </html>
         """
     d = Angango(angular_template)
     d.parse_all()
-    print d
+    # print d
